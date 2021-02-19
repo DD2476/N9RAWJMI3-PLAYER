f962761c4c0afc5d9312713aabc8455ee3bf1726
DD2476 <DragosDragos2476@gmail.com>
2021-02-19 21:12:10 +0200

    update

--- N9RAWJMI3 PLAYER/Form1.Designer.vb
+++ N9RAWJMI3 PLAYER/Form1.Designer.vb
@@ -22,10 +22,137 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.btnBack = New System.Windows.Forms.Button()
+        Me.btnPlayPause = New System.Windows.Forms.Button()
+        Me.btnForward = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.always = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Location = New System.Drawing.Point(12, 93)
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(187, 45)
+        Me.TrackBar1.TabIndex = 0
+        Me.TrackBar1.TickStyle = System.Windows.Forms.TickStyle.TopLeft
+        '
+        'btnBack
+        '
+        Me.btnBack.Font = New System.Drawing.Font("Segoe UI Symbol", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBack.Location = New System.Drawing.Point(12, 12)
+        Me.btnBack.Name = "btnBack"
+        Me.btnBack.Size = New System.Drawing.Size(50, 50)
+        Me.btnBack.TabIndex = 1
+        Me.btnBack.Text = "⏮"
+        Me.btnBack.UseVisualStyleBackColor = True
+        '
+        'btnPlayPause
+        '
+        Me.btnPlayPause.Font = New System.Drawing.Font("Segoe UI Symbol", 24.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPlayPause.Location = New System.Drawing.Point(68, 12)
+        Me.btnPlayPause.Name = "btnPlayPause"
+        Me.btnPlayPause.Size = New System.Drawing.Size(75, 75)
+        Me.btnPlayPause.TabIndex = 2
+        Me.btnPlayPause.Text = "▶"
+        Me.btnPlayPause.UseVisualStyleBackColor = True
+        '
+        'btnForward
+        '
+        Me.btnForward.Font = New System.Drawing.Font("Segoe UI Symbol", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnForward.Location = New System.Drawing.Point(149, 12)
+        Me.btnForward.Name = "btnForward"
+        Me.btnForward.Size = New System.Drawing.Size(50, 50)
+        Me.btnForward.TabIndex = 3
+        Me.btnForward.Text = "⏭"
+        Me.btnForward.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Segoe UI Symbol", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(12, 144)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(30, 30)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "⏏"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Segoe UI Symbol", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(48, 144)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(30, 30)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "ℹ"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("Segoe UI Symbol", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(84, 144)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(30, 30)
+        Me.Button6.TabIndex = 6
+        Me.Button6.Text = "≡"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.N9RAWJMI3_PLAYER.My.Resources.Resources.ca5b27f76505383800dafd65ca2a9d7a
+        Me.PictureBox1.Location = New System.Drawing.Point(151, 123)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(50, 50)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 7
+        Me.PictureBox1.TabStop = False
+        '
+        'always
+        '
+        Me.always.Enabled = True
+        Me.always.Interval = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(213, 185)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.btnForward)
+        Me.Controls.Add(Me.btnPlayPause)
+        Me.Controls.Add(Me.btnBack)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "N9RAWJMI3 PLAYER"
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents btnBack As Button
+    Friend WithEvents btnPlayPause As Button
+    Friend WithEvents btnForward As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents always As Timer
 End Class
--- N9RAWJMI3 PLAYER/Form1.vb
+++ N9RAWJMI3 PLAYER/Form1.vb
@@ -1,3 +1,39 @@
 ﻿Public Class Form1
 
+    Public isPlaying As Boolean = False
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnPlayPause.Click
+
+        If isPlaying = True Then
+
+            isPlaying = False
+
+        ElseIf isPlaying = False Then
+
+            isPlaying = True
+
+        End If
+
+    End Sub
+
+    Private Sub always_Tick(sender As Object, e As EventArgs) Handles always.Tick
+
+        If isPlaying = True Then
+
+            btnPlayPause.Text = "⏸"
+
+        ElseIf isPlaying = False Then
+
+            btnPlayPause.Text = "▶"
+
+        End If
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+        MessageBox.Show("N9RAWJMI3 PLAYER: Made by DD2476 for N9RAWJMI3.", "Credits", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+    End Sub
+
 End Class
